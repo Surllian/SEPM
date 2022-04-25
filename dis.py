cf61caa086a5ffacd3e81636c76dfaba8b37f2f5
Surllian <73808762+Surllian@users.noreply.github.com>
2022-04-25 22:44:39 +0700

    dis.py

---
+++ dis.py
@@ -0,0 +1,88 @@
+if district_to_view == "District 12":
+    m = folium.Map(location=[])
+
+    folium.Marker(
+        [10.872409787197864, 106.67221737421346], popup="LongBuri\nRating: 4.15", tooltip="LongBuri"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.82910187231742, 106.62703009803333], popup="Nha hang Dai Phu\nRating: 3.85", tooltip="Nha hang Dai Phu"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.853792561353314, 106.62554689803358], popup="Moda House coffee\nRating: 4", tooltip="Moda House coffee"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.84642257284936, 106.63345351337806], popup="Met Quan\nRating: 3.3", tooltip="Met Quan"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.827551497626137, 106.61912037105027], popup="Kimbab tuoi\nRating: 3.7", tooltip="Kimbab Tuoi"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.853237846203227, 106.6779545422142], popup="Khu du lich Ben Xua\nRating: 3.3", tooltip="Khu du lich Ben Xua"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.828876061419255, 106.62999652501651], popup="Quan an Han Quoc\nRating: 3.35", tooltip="Quan An Han Quoc"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.830181673603828, 106.62308541152491], popup="Sky 17 cafe\nRating: 3.2", tooltip="Sky 17 cafe"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.8694406466983, 106.64863164036132], popup="Meo beo tiramisu\nRating: 4.4", tooltip="Meo beo tiramisu"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.86661584958722, 106.61584561152524], popup="Zolo Tea\nRating: 4.3", tooltip="Zolo Tea"
+    ).add_to(m)
+
+if district_to_view == "District 11":
+    m = folium.Map(location=[])
+
+    folium.Marker(
+        [10.761315796287489, 106.65085612872197], popup="Tra sua bon bon\nRating: 3.95", tooltip="Tra sua bon bon"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.767839050977939, 106.65317056919677], popup="Ha cao pha lau Marie Curie\nRating: 3.7", tooltip="Ha cao pha lau Marie Curie"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.853792561353314, 106.62554689803358], popup="Moda House coffee\nRating: 4", tooltip="Moda House coffee"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.758153797690815, 106.65266498454126], popup="Sui cao Ngoc Y\nRating: 3.35", tooltip="Sui cao Ngoc Y"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.757928475625025, 106.64483885570515], popup="Sapinkie\nRating: 3.15", tooltip="Sapinkie"
+    ).add_to(m)
+
+if district_to_view == "District 11":
+    m = folium.Map(location=[])
+
+    folium.Marker(
+        [10.77228846734884, 106.66966379803287], popup="Hanuri\nRating: 3.5", tooltip="Hanuri"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.771541467924596, 106.67005998083548], popup="Monkey in black\nRating: 3.8", tooltip="Monkey in black"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.77263474778628, 106.66946864221367], popup="Say coffee\nRating: 3.45", tooltip="Say coffee"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.773503265094195, 106.66870619803296], popup="Cow Express\nRating: 3.85", tooltip="Cow Express"
+    ).add_to(m)
+
+    folium.Marker(
+        [10.766473054466099, 106.67419933239366], popup="Lau tom cang xien Tran Nhan Ton\nRating: 3.5", tooltip="Lau tom cang xien Tran Nhan Ton"
+    ).add_to(m)
