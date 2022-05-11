740c7e804d8bafdfc107592ab73fc90d456d8a6d
Surllian <73808762+Surllian@users.noreply.github.com>
2022-05-12 03:52:56 +0700

    Update and rename test2.py to result.py

--- result.py
+++ result.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import networkx as nx
 import leafmap.foliumap as leafmap
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 
+st.set_page_config(layout="wide")
 
 components.html(
     """
@@ -46,8 +46,8 @@ components.html(
             .myNav li a {
                 display: inline-block;
                 text-align: right;
-                padding: 14px 8px;
-                font-size: 20px;
+                padding: 14px 14px;
+                font-size: 30px;
                 font-family: Arial;
             }
             .myNav a:link { text-decoration: none; 
@@ -198,17 +198,17 @@ components.html(
     """,width=None, height=None,scrolling=False,
 )
 with st.container():
-    st.markdown('<h1 style:"padding:-15 0;">RMIT</h1>', unsafe_allow_html=True)
+    st.markdown("<p style='text-align: left; font-size:60px; font-family: Arial;'><b>RMIT - Ho Chi Minh brand</b></p>", unsafe_allow_html=True)
     st.markdown("""---""")
 with st.container():
-    col1,col2,col3,col4 = st.columns([1,0.75,1.2,3.6])
-    col1.markdown("<p style='text-align: left'>Vietnamese |</p>", unsafe_allow_html=True)
-    col2.markdown("<p style='text-align: left'>School |</p>", unsafe_allow_html=True)
-    col3.markdown("<p style='text-align: left'>Avg: 100.000vnd |</p>", unsafe_allow_html=True)
-    col4.markdown('<p style="text-align: left"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="yellow" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/></svg> 4.4 </p>', unsafe_allow_html=True)
-st.subheader("Address")
-st.write("702 Nguyễn Văn Linh, Tân Hưng, Quận 7, Thành phố Hồ Chí Minh 700000, Việt Nam")
-components.iframe("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.063744373333!2d106.69088691545974!3d10.729566992353336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fbea5fe3db1%3A0xfae94aca5709003f!2zxJDhuqFpIGjhu41jIFJNSVQgVmnhu4d0IE5hbSAtIGPGoSBz4bufIE5hbSBTw6BpIEfDsm4!5e0!3m2!1svi!2s!4v1652137475076!5m2!1svi!2s",height=600,)
+    col1,col2,col3,col4 = st.columns([0.75,0.5,1,9.75])
+    col1.markdown("<p style='text-align: left; font-size:30px;'>Vietnamese |</p>", unsafe_allow_html=True)
+    col2.markdown("<p style='text-align: left; font-size:30px;'>School |</p>", unsafe_allow_html=True)
+    col3.markdown("<p style='text-align: left; font-size:30px;'>Avg: 100.000vnd |</p>", unsafe_allow_html=True)
+    col4.markdown('<p style="text-align: left; font-size:30px;"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="yellow" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg> 4.4 </p>', unsafe_allow_html=True)
+st.header("Address")
+st.markdown("<p style='text-align: left; font-size:25px;'>702 Nguyễn Văn Linh, Tân Hưng, Quận 7, Thành phố Hồ Chí Minh 700000, Việt Nam</p>", unsafe_allow_html=True)
+components.iframe("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.063744373334!2d106.69088691533346!3d10.729566992353291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752fbea5fe3db1%3A0xfae94aca5709003f!2zxJDhuqFpIGjhu41jIFJNSVQgVmnhu4d0IE5hbSAtIGPGoSBz4bufIE5hbSBTw6BpIEfDsm4!5e0!3m2!1svi!2s!4v1652287432698!5m2!1svi!2s",height=1000,)
 components.html(
     """
     <!DOCTYPE html>
@@ -242,7 +242,7 @@ components.html(
             .column2 {
                 float: left;
                 background-color:rgb(255, 255, 255);
-                height: 50px;
+                height: 75px;
                 width: 100%;
             }
 
@@ -272,6 +272,7 @@ components.html(
                 left: 0;
                 bottom: 0;
                 width: 100%;
+                heitht: 100%;
                 background-color: rgb(0, 0, 0);
                 color: white;
                 text-align: left;
@@ -292,7 +293,7 @@ components.html(
                 display: inline-block;
                 text-align: left;
                 padding: 14px 16px;
-                font-size: 18px;
+                font-size: 25px;
                 font-family: Arial;
             }
             .column a:link { text-decoration: none; 
@@ -319,7 +320,7 @@ components.html(
                 display: block;
                 text-align: right;
                 padding: 14px 16px;
-                font-size: 18px;
+                font-size: 25px;
                 font-family: Arial;
             }
 
@@ -355,7 +356,7 @@ components.html(
                 </div>
             </div>
             <div class="column2">
-                <p style="text-align: right;font-size: 20px; padding: 0 14px; color: black;"><b>Copyright © 2022 DAVIKO TEAM</b></p>
+                <p style="text-align: right;font-size: 30px; padding: 0 14px; color: black;"><b>Copyright © 2022 DAVIKO TEAM</b></p>
             </div>
         </div>
     </body>
