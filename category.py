e3d7d4c55580ac01ecf609c0012cfce999ddfbe4
MiuX <s3911365@rmit.edu.vn>
2022-05-12 20:48:52 +1000

    Update category.py

--- category.py
+++ category.py
@@ -16,6 +16,48 @@ from pyecharts.faker import Faker
 from pyecharts.globals import ThemeType
 import streamlit.components.v1 as components
 
+# Navigation_bar
+# Hide menu
+hide_menu = """
+<style>
+#MainMenu { visibility: hidden;
+}
+</style>
+"""
+st.markdown(hide_menu, unsafe_allow_html=True)
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+#Header
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #FFFFFF; border-bottom-style: solid;">
+  <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/277445185_695956418275236_1324434635958245909_n.png?stp=dst-png_p206x206&_nc_cat=108&ccb=1-5&_nc_sid=aee45a&_nc_ohc=HJhaOg0gRMMAX-Bjzjx&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIVfpGRprjvJVO3UZSSaT0ygvyDOXCUffjGu-9qD9BCKA&oe=627D7229" 
+            class="rounded float-left" alt="MAFOOD" width="50" height="50">
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank" style="color: #000000;">MAFOOD</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#" style="color: #000000;"> Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank" style="color: #000000;">Cuisine</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank" style="color: #000000;">Category</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost:8502" target="_blank" style="color: #000000;">Location</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank" style="color: #000000;">Reccomendation</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
 st.title('Category')
 st.caption('This page shows the data by category of restaurants: buffet, cafe, dine out, bar, and pub. It includes distribution charts by type of restaurant, average rating and rating distribution graphs, and graphs showing up to 10 restaurants by category.')
@@ -707,3 +749,20 @@ components.html(
 </body>
 """,height=500
 )
+
+# Footer
+st.markdown("""
+<nav class="navbar fixed-bottom navbar-expand-xl navbar-dark" style="background-color: #FFFFFF; border-top-style: solid; height: 80px">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank" style="color: #000000;">About Us</a>
+      </li>
+    </ul>
+  </div>
+  <div style="padding: 100px 80px 50px">Copyright ©2022 TEAM DAVIKO</div>
+</nav>
+""", unsafe_allow_html=True)
